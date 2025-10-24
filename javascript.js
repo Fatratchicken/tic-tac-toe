2b30bdb8e642fe0ca285f6e67981f8355089f65b
moshe <moshebekritsky@gmail.com>
2025-10-24 09:09:44 -0400

    Made Game board object.
    Factory function IIFE for single modular use.

---
+++ javascript.js
@@ -0,0 +1,28 @@
+//IIFE factory function for the gameboard:
+
+const GameBoard = (function (){
+    const board = [
+        ['','',''],
+        ['','',''],
+        ['','','']
+    ];
+
+    const getBoard = () => board;
+
+    const placePiece = function(index_x, index_y, player_symbol){
+        board[index_y][index_x] = player_symbol;
+    }
+
+    const resetBoard  = () => {
+        for(let i = 0; i < board.length; i++){
+            for (let j = 0; j < board.length; j++){
+                board[i][j] = '';
+            }
+        }
+    }
+    
+    return {getBoard, placePiece, resetBoard}
+}());
+
+
+
