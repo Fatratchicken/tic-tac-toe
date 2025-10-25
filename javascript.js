55563b5bb5d5bc0aa08f0d59be4d50ac21e6d02e
moshe <moshebekritsky@gmail.com>
2025-10-25 14:44:57 -0400

    Added check for cross wins.
    "cross win" function.

--- javascript.js
+++ javascript.js
@@ -2,9 +2,9 @@
 
 const GameBoard = (function (){
     const board = [
-        ['','',''],
-        ['','',''],
-        ['','','']
+        ['','','O'],
+        ['','O',''],
+        ['O','','O']
     ];
 
     const getBoard = () => board;
@@ -32,6 +32,32 @@ const GameBoard = (function (){
         }, 0);
 
         if (row_win % 2 == 0){
+            return true;
+        }
+
+        //check for x cross win: 
+        const cross_win = function() {
+            let cross_length_one = 0;
+            let cross_length_two = 0;
+
+            for (let i = 0; i < board.length; i++){
+                if (board[i][i] === player_symbol){
+                    cross_length_one ++;
+                }
+
+                if (board[(board.length-1)- i][i] === player_symbol){
+                    cross_length_two ++;
+                }
+            }
+
+            if (cross_length_one === board.length || cross_length_two === board.length){
+                return true;
+            }
+
+            return false;
+        }
+
+        if (cross_win()){
             return true;
         }
 
